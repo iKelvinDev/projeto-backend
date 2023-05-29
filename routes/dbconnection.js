45686e9d5016afa3146bedcd337e4a10b68675a5
Kelvin <kelvinmarquesk@gmail.com>
2023-05-29 16:08:19 -0300

    Criado o arquivo dbconnection.js na pasta de rotas para reutilização de conexão no database

---
+++ routes/dbconnection.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'dblojahardware'
+});
+
+con.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Conexão com o banco de dados estabelecida com sucesso!');
+});
+
+module.exports = con;
